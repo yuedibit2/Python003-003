c37780828f398d8a36234245481416d6619a0b24
lijunli <yuedibit2@126.com>
2020-09-25 09:12:35 +0800

    modify week01

--- week01/homework_2/spiders/spiders/pipelines.py
+++ week01/homework_2/spiders/spiders/pipelines.py
@@ -9,14 +9,20 @@ from itemadapter import ItemAdapter
 #import pandas as pd
 
 class SpidersPipeline:
+    def open_spider(self,spider):
+        self.file=open('./maoyan_movie.csv', 'a', encoding='gbk')
+
     def process_item(self, item, spider):
         print(item)
         movie_name = item['movie_name']
         movie_categorys = item['movie_categorys']
         plan_time = item['plan_time']
         output = f'{movie_name},{movie_categorys},{plan_time}\n'
-        with open('./maoyan_movie.csv', 'a+', encoding='gbk') as article:
-            article.write(output)
+        self.file.write(output)
 
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
+
 
